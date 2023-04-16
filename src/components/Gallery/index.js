24f6818fdbeef0f296cc98ac7d60ca6bc73d8e93
bekzatkazatov <bekzatx2x@gmail.com>
2023-04-16 22:51:10 +0600

    first user

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -6,25 +6,43 @@ import React, {useState} from 'react';
 //             https://swiperjs.com/demos/images/nature-4.jpg
 
 
-
 const Gallery = () => {
-    const [pickedimg,setPickedimg] = useState([0])
+    const [pickedimg, setPickedimg] = useState(imgs[0])
+
+    const getIndex = () => {
+        return  imgs.indexOf(pickedimg)
+    }
+    const prev = () => {
+        setPickedimg(imgs[getIndex() - 1])
+    }
+
+    const next = () => {
+        setPickedimg(imgs[getIndex() + 1])
+    }
 
     return (
-        <div style={{marginTop:'20px',
-             display:'flex',
-              flexDirection:'column',
-              justifyContent:'center',
-               alignItems:'center'}}>
+        <div style={{
+            marginTop: '10px',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
             <div style={{
-                height:'300px'
+                height: '395px'
             }}>
-                <img width={'600px'} height={'300px'} src={pickedimg.src} alt=""/>
+                <div>
+                    <button className={'buttonPrev'} disabled={imgs.indexOf(pickedimg) === 0}  onClick={prev}> ❮ </button>
+                    <button className={'buttonNext'} disabled={imgs.indexOf(pickedimg) === imgs.length - 1} onClick={next}>❯ </button>
+                </div>
+                <img          width={'600px'} height={'350px'} src={pickedimg.src} alt=""/>
+
             </div>
-            <div style={{display:'flex',textAlign:'center'}}>
+            <div style={{display: 'flex', textAlign: 'center'}}>
                 {
-                    imgs.map(el=>
-                        <div style={{border:'1px solid',marginBottom:'20px'}} key={el.id} onClick={()=>setPickedimg(el)}>
+                    imgs.map(el =>
+                        <div style={{border: '1px solid', marginBottom: '20px'}} key={el.id}
+                             onClick={() => setPickedimg(el)}>
                             <img width={'148px'} height={'110px'} src={el.src} alt=""/>
                         </div>
                     )
@@ -35,25 +53,21 @@ const Gallery = () => {
 };
 
 
-
-
-
-
-const imgs =[
+const imgs = [
     {
-        id:'1',
+        id: '1',
         src: 'https://swiperjs.com/demos/images/nature-1.jpg'
     },
     {
-        id:'2',
+        id: '2',
         src: 'https://swiperjs.com/demos/images/nature-2.jpg'
     },
     {
-        id:'3',
+        id: '3',
         src: 'https://swiperjs.com/demos/images/nature-3.jpg'
     },
     {
-        id:'4',
+        id: '4',
         src: 'https://swiperjs.com/demos/images/nature-4.jpg'
     }
 ]
